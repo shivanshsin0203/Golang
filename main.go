379e5b103fe95b406838df49c90c6ffca98a5baf
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-16 16:02:08 +0530

    Time format in go

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"bufio"
+	
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	
+	"time"
 )
 
 func main() {
-	fmt.Println("Welcome to pizza app")
-	fmt.Println("Give ratinng to pizza")
-	reader :=bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	fmt.Println("Thanks for rating, ", input)
-	numRating,err :=strconv.ParseFloat(strings.TrimSpace(input),64)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Added 1 to your rating: ", numRating+1)
-	}
+	fmt.Println("Learn Go - 1.1")
+	date := time.Now()
+	fmt.Println("Today is", date)
+	fmt.Println(date.Format("02-01-2006 15:04:05 Monday"))
 }
