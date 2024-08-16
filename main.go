cc82e08860ec8b6bd650146414dfaacebf0b73b6
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-16 15:56:34 +0530

    parsing in go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,4 +14,10 @@ func main() {
 	reader :=bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
 	fmt.Println("Thanks for rating, ", input)
+	numRating,err :=strconv.ParseFloat(strings.TrimSpace(input),64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating: ", numRating+1)
+	}
 }
