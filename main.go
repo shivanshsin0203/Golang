0c5026209076aaea9fee756c88c46afba72f512a
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-19 17:41:22 +0530

    Structs in golang

--- main.go
+++ main.go
@@ -7,21 +7,14 @@ import (
 )
 
 func main() {
-    fmt.Println("Maps in Golang")
-	 languages := make(map[string]string)
-	languages["JS"] = "Javascript"
-	languages["PY"] = "Python"
-	languages["RB"] = "Ruby"
-	languages["GO"] = "Golang"
-
-	fmt.Println("Languages: ", languages)
-
-	delete(languages, "RB")
-	fmt.Println("Languages: ", languages["PY"])
-
-	// Iterating over a map
-	for key, value := range languages {
-		fmt.Printf("%s: %s\n", key, value)
-	}
+   fmt.Println("Stgructs in go lang")
+     shivansh :=User{"Shivansh","singhshivansh12may@gmail.com",21,true}
+	fmt.Println(shivansh)
 	
+}
+type User struct {
+	Name string
+	Email string
+	Age int
+	Status bool
 }
