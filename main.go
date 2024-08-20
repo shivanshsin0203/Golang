979961c985e7fa0c77539bc3cbc0ec5973e2965f
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-20 19:10:27 +0530

    Different loop format

--- main.go
+++ main.go
@@ -9,10 +9,28 @@ func main() {
      fmt.Println("Lopps in golang")
 	 days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
 
-	 for i := 0; i < len(days); i++ {
-		 fmt.Println(days[i])
+	//  for i := 0; i < len(days); i++ {
+	// 	 fmt.Println(days[i])
+	//  }
+	//  for i:= range days {
+	// 	 fmt.Println(days[i])
+	//  }
+	 for index, value := range days {
+		 fmt.Printf("Index: %v, Value: %v\n", index, value)
 	 }
-	 for i:= range days {
-		 fmt.Println(days[i])
+	  value :=1
+
+	 for value < 10 {
+        if value ==3{
+			goto lco
+		}
+		if(value == 5){
+			value++
+			continue
+		}
+		 fmt.Println(value)
+		 value++
 	 }
+	 lco:
+	     fmt.Println("Jumping to LCO")
 }
