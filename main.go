ec173bf88ad939c4f8129f40f5ec6f830392d3a1
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-22 14:20:35 +0530

    Meathods in go

--- main.go
+++ main.go
@@ -6,19 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Meathods in Go")
-     shivam := User{"Shivam","Kumar","awd@gmail.com",21,true}
-	 fmt.Println("User is",shivam)
-	 shivam.GetStatus()
-}
-
-type User struct{
-	FirstName string
-	LastName string
-	Email string
-	Age int
-	isActive bool
-}
-func (u User) GetStatus() {
-	fmt.Println("Is user active",u.isActive)
+	fmt.Println("Defer in Go")
+    
+	defer fmt.Println("This is the first defer statement")
+	defer fmt.Println("This is the second defer statement")
+	fmt.Println("Hello, World!")
 }
