f4c94badcad76b14c77c0f5c80f3daae931b9f0a
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-22 15:13:32 +0530

    Webrequests in go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-
+	"os"
 )
 const url = "https://nith.ac.in"
 func main() {
@@ -19,4 +19,15 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(data))
+	content := string(data)
+	file, err := os.Create("nith.html")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	length,err := io.WriteString(file,content)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File created with %v characters",length)
 }
