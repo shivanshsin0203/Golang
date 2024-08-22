c96edd1a5c3d9a3b4744a0d4971a5cb59bcd34dd
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-22 14:36:04 +0530

    Read and Write file in go

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	
+	"os"
 )
 
 func main() {
-	fmt.Println("Defer in Go")
-    
-	defer fmt.Println("This is the first defer statement")
-	defer fmt.Println("This is the second defer statement")
-	fmt.Println("Hello, World!")
+	fmt.Println("Files in golang")
+	content := "Hello, World!"
+	file,err := os.Create("./test.txt")
+	if err != nil {
+		panic(err)
+	}
+	length,err := io.WriteString(file,content)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Length: %d",length)
+	readFile("./test.txt")
+	defer file.Close()
+}
+func readFile (fileName string) {
+	data,err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("The data read from the file is: ",string(data))
 }
