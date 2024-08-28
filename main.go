95c171304e33f06979a955949ca393a3c5fc6032
Shivansh Singh <singhshivansh12may@gmail.com>
2024-08-28 09:57:33 +0530

    Controller functions in go

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Course struct{
@@ -17,4 +21,25 @@ type Author struct{
 var courses []Course
 func main(){
 	fmt.Println("Making api in go")
+}
+// Controller function
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to the home page</h1>"))
+}
+func getAllCourses(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+func getOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+	params :=mux.Vars(r)
+	for _, item := range courses{
+		if item.CourseId == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Course not found")
 }
